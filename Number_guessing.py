a166ed5afaf1a76c704dd347549a318664effa05
Aravind Balachandran <125515856+aravindbc@users.noreply.github.com>
2023-08-20 16:41:53 +0530

    Add files via upload

---
+++ Number_guessing.py
@@ -0,0 +1,55 @@
+import random
+
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+    if top_of_range <=0:
+        print("Please type a number larger than 0 next time.")
+        quit()
+else:
+    print("Please type a number next time.")
+    quit()
+
+random_number = random.randrange(0, top_of_range)
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number next time.")
+        continue
+    
+    if user_guess == random_number:
+        print("You got it!")
+        break
+    elif user_guess > random_number:
+        print("you were above the number!")
+    else:
+        print("You were below the number!")
+
+print("You got it in", guesses, "guesses")
+
+
+
+# Simple version
+""""
+import random
+
+n = random.randrange(1,10)
+guess = int(input('Enter any number: '))
+
+while n != guess:
+    if guess < n:
+        print('Too low')
+        guess = int(input('Enter number again: '))
+    elif guess > n:
+        print('Too high')
+        guess = int(input('Enter number again: '))
+    else:
+        break
+print('You guessed it right')
+"""
